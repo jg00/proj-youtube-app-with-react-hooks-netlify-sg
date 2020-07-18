26e5df776d59d4baf33dcbf8871990a25f7b09f7
Jack Gamboa <jgamboa929@gmail.com>
2020-07-18 00:55:50 -0500

    build

---
+++ build/precache-manifest.4a59563f21d37ca79b3fd5ff9bbd2505.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde68faaaafea6c37687b0940b16e61f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd276b5aa6d14e8cc32e",
+    "url": "/static/css/main.600540da.chunk.css"
+  },
+  {
+    "revision": "e71189034bc90da61e08",
+    "url": "/static/js/2.3c8365db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3c8365db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd276b5aa6d14e8cc32e",
+    "url": "/static/js/main.39d10c65.chunk.js"
+  },
+  {
+    "revision": "8c0ed676e7fc0e04cf11",
+    "url": "/static/js/runtime-main.81dc9e71.js"
+  }
+]);
